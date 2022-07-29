4c69b793c941159463cf03ac4396ec99b5e63dd6
Estevão Almeida <estevaorimes@gmail.com>
2022-07-29 17:21:57 -0300

    .

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Button, Alert, View } from 'react-native';
+import Main from './Pages/Main';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Button onPress={()=>Alert.alert('Login')}title='Login'/>
+      <Main usuario='Maximus' horas='08:00' temp='18'/>
+      {/* <Button onPress={()=>Alert.alert('Login')}title='Login'/>
       <Button onPress={()=>Alert.alert('Cadastro')}title='Cadastro'/>
-      <StatusBar style="auto" />
+      <StatusBar style="auto" /> */}
     </View>
   );
 }
--- Pages/Main.js
+++ Pages/Main.js
@@ -1,33 +1,55 @@
-import { View, Text, Button, Alert} from 'react-native'
-import React from 'react'
+import {StyleSheet, View, Button, Alert} from 'react-native';
+import styled from 'styled-components';
+import React from 'react';
 
+const Row = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-itens: center;
+    width: 100%;
+`
+const Column = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+const Title = styled.div`
+    font-size: 24px;
+    font-weight: 700px;
+`
+const Text = styled.div`
+    font-size: 18px;
+    font-weight: 400px;
+`
 
-const Main = ({Usuário, Horas, Temperatura}) => {
+const Main = ({usuario, horas, temp}) => {
   return (
-    <View>
-        <div style={main.title}>
-            <Text>Bom dia, {Usuário}!</Text>
-            <Text>São {Horas} e faz {Temperatura} Graus.</Text>
-        </div>
-        <div>
-            <Button onPress={()=>Alert.alert('Formulário')}title='Ir para o Formulário'/>
-            <Button onPress={()=>Alert.alert('Importar PDF')}title='Importar PDF'/>
-            <Button onPress={()=>Alert.alert('Lista')}title='Lista de Vendas'/>
-            <Button onPress={()=>Alert.alert('Instagram')}title='Instagram'/>
-        </div>
-        <div>
-            <Button onPress={()=>Alert.alert('Animação')}title='Animate'/>
-        </div>
-        
+    <View >
+        <Container>
+            <Column>
+                <Title>Bom dia, {usuario}!</Title>
+                <Text>São {horas} e está fazendo {temp} graus</Text>
+            </Column>
+            <Row>
+                <Button onPress={()=>Alert.alert('teste')} title='form'/>
+                <Button onPress={()=>Alert.alert('teste')} title='import pdf'/>
+            </Row>
+            <Row>
+                <Button onPress={()=>Alert.alert('teste')} title='vendas'/>
+                <Button onPress={()=>Alert.alert('teste')} title='insta'/>
+            </Row>
+            <Row>
+                <Button style={{width:'100%'}} onPress={()=>Alert.alert('teste')} title='animate'/>
+            </Row>
+        </Container>
     </View>
   )
 }
 
-const main = StyleSheet.create({
-    title:{
-        flex: 1,
-        
-    }
-})
 
 export default Main
+
